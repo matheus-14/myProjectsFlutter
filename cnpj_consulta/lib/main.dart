9cdfa8d7d71a080a8cac207f3a30b470b0dd7e4b
Matheus Souza <matheussouza2015@yahoo.com.br>
2024-02-20 13:13:39 -0300

    final 20/02
    tema escuro

--- cnpj_consulta/lib/main.dart
+++ cnpj_consulta/lib/main.dart
@@ -17,9 +17,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: const MyHomePage(),
     );
   }
 }
